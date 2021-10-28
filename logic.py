0f9f724c13594ac690f533091fda1e3571d19bd8
McTuTuM <marchenko.k@interset.su>
2021-10-28 14:50:28 +0300

    Hi:)

---
+++ logic.py
@@ -0,0 +1,49 @@
+from PyQt5 import QtWidgets
+from text_reader import Ui_MainWindow
+import easyocr
+
+class Window(Ui_MainWindow,QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.addFunctions()
+
+
+    def addFunctions(self):
+        self.pushButton.clicked.connect(lambda:self.showDialog())
+        self.pushButton_2.clicked.connect(lambda:self.saveDialog())
+        self.pushButton_3.clicked.connect(lambda:self.main())
+
+    def text_recognition(self, file_path,text_file_name):
+        reader = easyocr.Reader(["ru"])
+        result = reader.readtext(file_path, detail= 0, paragraph= True)
+        with open(text_file_name, "w") as file:
+            for line in result:
+                file.write(f"{line}\n\n")
+        return f"Result wrote into {text_file_name}"
+
+    def main(self):
+        file_path = self.lineEdit.text()
+        text_file_name = self.lineEdit_2.text()
+        self.text_recognition(file_path=file_path, text_file_name=text_file_name)
+        
+    def showDialog(self):
+        fname = QtWidgets.QFileDialog.getOpenFileName(self, "Open file", "C:\cod\TextReader")[0]
+        try:
+            f = open(fname, 'r')
+            with f:
+                # data = f.read()
+                self.lineEdit.setText(fname)
+                f.close()
+        except:
+            pass
+    def saveDialog(self):
+        fname = QtWidgets.QFileDialog.getSaveFileName(self, "Save file", "C:\cod\TextReader")[0]
+        try:
+            f = open(fname, 'w')
+            with f:
+                # data = f.read()
+                self.lineEdit_2.setText(fname)
+                f.close()
+        except:
+            pass
